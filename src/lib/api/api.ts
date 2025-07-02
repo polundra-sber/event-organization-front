3d5ad844de337330e272b5677fdfe1c86020352f
Anastasiya <Lozhkina_Anastasya@list.ru>
2025-07-02 17:05:36 +0300

    feat: add mocks end endpoints for my-tasks

--- src/lib/api/api.ts
+++ src/lib/api/api.ts
@@ -13,7 +13,7 @@ export const api = createApi({
       return headers;
     },
   }),
-  tagTypes: ["Event", "MyStuff"],
+  tagTypes: ["Event", "MyStuff", "MyTask"],
   endpoints: () => ({}),
 });
 
--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -2,5 +2,6 @@ import { eventHandlers } from "./events";
 import { authHandlers } from "./auth";
 import { profileHandlers } from "./profile";
 import { myStuffHandlers } from "./my-stuffs-mocks"
+import { myTaskHandlers } from "./my-tasks-mocks";
 
-export const handlers = [...myStuffHandlers, ...eventHandlers, ...authHandlers, ...profileHandlers ];
+export const handlers = [...myStuffHandlers, ...myTaskHandlers, ...eventHandlers, ...authHandlers, ...profileHandlers ];
