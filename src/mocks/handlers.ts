bac91f0d7f35d81fd4609ab0e82491501de615a9
Anastasiya <Lozhkina_Anastasya@list.ru>
2025-07-02 11:23:41 +0300

    feat: add mocks and endpoits for my-stuff

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,5 +1,6 @@
 import { eventHandlers } from "./events";
 import { authHandlers } from "./auth";
 import { profileHandlers } from "./profile";
+import { myStuffHandlers } from "./my-stuffs-mocks"
 
-export const handlers = [...eventHandlers, ...authHandlers, ...profileHandlers];
+export const handlers = [...myStuffHandlers, ...eventHandlers, ...authHandlers, ...profileHandlers ];
