06835e61cde9dbfb761dd4d1c3d3635f5db157d7
Anastasiya <Lozhkina_Anastasya@list.ru>
2025-07-04 11:10:00 +0300

    feat: create page for my-debts

---
+++ src/pages/my-debts/index.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+import { MyDebtsPageContent } from "@/components/myDebts/MyDebtsPage";
+
+export default function MyDebtsPage() {
+  const router = useRouter();
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (!token) {
+      router.push("/");
+    } else {
+      setIsLoading(false);
+    }
+  }, [router]);
+
+  if (isLoading) {
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-pink-500"></div>
+      </div>
+    );
+  }
+
+  return <MyDebtsPageContent />;
+}
