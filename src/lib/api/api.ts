10b78ce593bdd17935798334846d39e5bb14da5a
tutibase <dykun1784@gmail.com>
2025-07-13 17:15:47 +0300

    feat: 401 error handling from the server

--- src/lib/api/api.ts
+++ src/lib/api/api.ts
@@ -1,18 +1,41 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import {
+  BaseQueryFn,
+  createApi,
+  FetchArgs,
+  fetchBaseQuery,
+} from "@reduxjs/toolkit/query/react";
+import { toast } from "sonner";
 
-// Создаем API клиент
+const baseQuery = fetchBaseQuery({
+  baseUrl: "/api",
+  prepareHeaders: (headers) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      headers.set("Authorization", `Bearer ${token}`);
+    }
+    return headers;
+  },
+});
+
+const baseQueryWithAuthCheck: BaseQueryFn<
+  string | FetchArgs,
+  unknown,
+  unknown
+> = async (args, api, extraOptions) => {
+  const result = await baseQuery(args, api, extraOptions);
+
+  if (result.error?.status === 401) {
+    localStorage.removeItem("token");
+    toast.error("Сессия истекла. Пожалуйста, войдите снова");
+    window.location.href = "/";
+  }
+
+  return result;
+};
+
 export const api = createApi({
   reducerPath: "api",
-  baseQuery: fetchBaseQuery({
-    baseUrl: "/api",
-    prepareHeaders: (headers) => {
-      const token = localStorage.getItem("token");
-      if (token) {
-        headers.set("Authorization", `Bearer ${token}`);
-      }
-      return headers;
-    },
-  }),
+  baseQuery: baseQueryWithAuthCheck, // Используем модифицированный baseQuery
   tagTypes: [
     "Event",
     "MyStuff",
