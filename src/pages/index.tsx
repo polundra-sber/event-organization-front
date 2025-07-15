1141d10d6005cc16f2a852e20ccc06b25333c4ff
tutibase <dykun1784@gmail.com>
2025-07-15 11:52:17 +0300

    feat: if the user has a token, it transfers him to the events list

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,26 @@
 import { LoginForm } from "@/components/auth/LoginForm";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import { Loader } from "@/components/common/Loader";
 
 export default function Home() {
+  const router = useRouter();
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+
+    if (token) {
+      // Если токен есть, перенаправляем на /events
+      router.push("/events");
+    } else {
+      // Если токена нет, останавливаем загрузку
+      setIsLoading(false);
+    }
+  }, [router]);
+
+  if (isLoading) {
+    return <Loader />;
+  }
   return <LoginForm />;
 }
